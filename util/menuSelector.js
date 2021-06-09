2ebfb542305612693cfc105579ed950a9e8ed89e
Ethan <Ethan@MBA1.fios-router.home>
2021-06-09 16:30:09 -0400

    finished menu selector

--- util/menuSelector.js
+++ util/menuSelector.js
@@ -1,22 +1,35 @@
+/**
+ * @typedef Track
+ * @property {String} url
+ * @property {String} name 
+ * @property {String} time 
+ * @property {String} thumbnail
+ */
+
 module.exports = class MenuSelector {
-    constructor(message, menuItems) {
+    constructor(uMessage, menuItems, format, execute) {
         this.items = menuItems;
-        this.message = message;
+        this.uMessage = uMessage;
         this.index = 0;
+        this.format = format.bind(this);
+        this.execute = execute.bind(this);
         this._update();
+        this.pages = [];
     }
-    up() {
+    up(button) {
         if(this.index!==(this.items.length-1)) {
             this.index +=1;
         } else {
             this.index = 0;
         }
         this._update();
+        button.defer();
     }
-    down() {
+    down(button) {
         if(!(this.index<=0)) this.index -= 1;
         else this.index = this.items.length-1;
         this._update();
+        button.defer();
     }
     /**
      * @description Redefine this
@@ -24,10 +37,16 @@ module.exports = class MenuSelector {
     execute(button) {
         
     }
-    format() {
-        return "N/A";
+    format(message) {
+        return {content: "N/A"};
     }
-    _update() {
+    async _update() {
         this.current = this.items[this.index];
+        let {content, embed , components} = this.format(this.message);
+        if(!this.message) {
+            this.message = await this.uMessage.channel.send(content, {embed: embed, components: [components], code: "diff"});
+        } else {
+            this.message.edit(content, {embed, components, code: "diff"});
+        }
     }
 }
