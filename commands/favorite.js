551b30fdb2f849eafbe348ed8c0e17881b657ab2
Ethan <Ethan@MBA1.fios-router.home>
2021-06-09 16:30:54 -0400

    override default methods

--- commands/favorite.js
+++ commands/favorite.js
@@ -1,5 +1,6 @@
 let Menu = require("../util/menuSelector");
 let Track = require("../util/track");
+let {MessageEmbed} = require("discord.js");
 module.exports = {
     name: "favorites",
     args: [{placeholder: "remove|list", description: "Must be remove or list"}, {placeholder: "index", description: "Index of the song to remove"}],
@@ -12,22 +13,31 @@ module.exports = {
             case "list": {
                 let favorites = client.favorites.get(message.author.id);
                 if(!favorites||!favorites.length) return message.channel.send("No favorited songs.");
-                let h = favorites.map((e, i)=>`${i+1}. ${e.name} | ${e.url}`).join("\n");
-                let m = new Menu(message, favorites);
-                m.execute = async (button) => {
-                    button.clicker.fetch();
-                    let queue = client.queue.get(this.message.id);
+                async function execute(button) {
+                    await button.clicker.fetch();
+                    let queue = client.queue.get(message.guild.id);
                     if(!queue) {
-                        if(!button.clicker.member.voice.channel.id) {
-                        }
                         let Queue = require("../util/serverQueue");
-                        client.menus.set(message.author.id, Queue(client, button.message.channel, member.guild, con, new Track(this.current.name, this.current.url, this.current.length, this.current.thumbnail)));
+                        if(!button.clicker.member.voice.channel) {button.message.channel.send("You need to be in a voice channel."); return button.defer();}
+                        let con = await button.clicker.member.voice.channel.join();
+                        client.queue.set(message.guild.id, new Queue(client, button.message.channel, button.clicker.member.guild, con, new Track(this.current.name, this.current.url, this.current.time, this.current.thumbnail)));
+                        button.defer();
                     } else {
-                        queue.addSong(this.current);
+                        queue.addSong(new Track(this.current.name, this.current.url, this.current.time, this.current.thumbnail));
+                        button.defer();
                     }
                 }
-                client.menus.favorites.set(message.author.id, new Menu(message, favorites));
-                message.channel.send("```\n"+h+"```");
+                function format (msg) {
+                    let {MessageActionRow, MessageButton} = require("discord-buttons");
+                    let components;
+                    if(!msg?.components) {components = new MessageActionRow().addComponents([new MessageButton().setLabel("Up").setID(`${message.author.id}_menuUp_favorites`).setStyle("blurple"),new MessageButton().setLabel("Down").setID(`${message.author.id}_menuDown_favorites`).setStyle("blurple"),new MessageButton().setLabel("Play").setID(`${message.author.id}_menuSelect_favorites`).setStyle("green")]);}
+                    else components = msg.components;
+                    let embed = new MessageEmbed().setThumbnail(this.current.thumbnail).setTitle(this.current.name).setURL(this.current.url);
+                    let content = this.items.map((e, i)=>`${this.index==i ? ">>> " : ""}${i+1}. ${e.name} | ${e.url}`).join("\n");
+                    return {content, embed, components}
+                }
+                let m = new Menu(message, favorites, format, execute);
+                client.menus.favorites.set(message.author.id, m);
                 break;
             }
             case "remove": {
@@ -35,8 +45,10 @@ module.exports = {
                 let favorites = client.favorites.get(message.author.id);
                 if(!favorites) return message.channel.send("No favorited songs.");
 
-                favorites.splice(parseInt(args[2])-1, 1);
+                let r = favorites.splice(parseInt(args[2])-1, 1);
+                let embed = new MessageEmbed().setThumbnail(r.thumbnail).setTitle("Removed: "+r.name).setURL(r.url).setColor("red");
                 client.favorites.set(message.author.id, favorites);
+                message.channel.send(embed);
                 break;
             }
             default: {
